622903966ca1cf0ac6ace6e0e30b5ea26b5bcd8a
Kaue Batista <kauecardozo3@gmail.com>
2024-11-21 12:05:35 -0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module br.com.fafic.virtualearn {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens br.com.fafic.virtualearn to javafx.fxml;
+    exports br.com.fafic.virtualearn;
+}
