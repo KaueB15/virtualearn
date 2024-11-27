03f8af848dd2fc0436c9f6e2e22b62339a6c99ca
Kaue Batista <kauecardozo3@gmail.com>
2024-11-27 15:25:45 -0300

    refact: update visual response

--- src/main/java/br/com/fafic/virtualearn/view/LoginViewController.java
+++ src/main/java/br/com/fafic/virtualearn/view/LoginViewController.java
@@ -3,10 +3,16 @@ package br.com.fafic.virtualearn.view;
 import br.com.fafic.virtualearn.controllers.LoginController;
 import br.com.fafic.virtualearn.dao.LoginDAO;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginViewController {
 
@@ -20,11 +26,14 @@ public class LoginViewController {
     PasswordField fieldPassword;
 
     @FXML
-    Button button;
+    Button signupButton;
+
+    @FXML
+    TextField fieldPasswordShow;
 
     LoginController loginController = new LoginController();
 
-    public void loginValidate(){
+    public void onLoginButtonClick(){
 
         String userName = fieldUsername.getText();
         String password = fieldPassword.getText();
@@ -35,6 +44,30 @@ public class LoginViewController {
             System.out.println("NÃO DEU CERTO");
         }
 
+    }
+
+    @FXML
+    public void onShowPassword(){
+        String password = fieldPassword.getText();
+        if (fieldPassword.isVisible()){
+            fieldPassword.setVisible(false);
+            fieldPasswordShow.setText(password);
+            fieldPasswordShow.setVisible(true);
+        }else{
+            fieldPasswordShow.setVisible(false);
+            fieldPassword.setText(password);
+            fieldPassword.setVisible(true);
+        }
+    }
+
+    @FXML
+    public void onSignupButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/pages/register-student-view.fxml"));
+        Parent loginRoot = fxmlLoader.load();
+        Stage stage = (Stage) signupButton.getScene().getWindow();
+        Pane mainPane = (Pane) stage.getScene().getRoot();
+        mainPane.getChildren().clear();
+        mainPane.getChildren().add(loginRoot);
     }
 
 }
