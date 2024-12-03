b40d4f6e5548668c3524a10137ad22ae3a3c86b9
Kaue Batista <kauecardozo3@gmail.com>
2024-12-03 01:43:36 -0300

    feat: teacher exception

---
+++ src/main/java/br/com/fafic/virtualearn/exceptions/TeacherNotRegisterException.java
@@ -0,0 +1,9 @@
+package br.com.fafic.virtualearn.exceptions;
+
+public class TeacherNotRegisterException extends RuntimeException{
+
+    public TeacherNotRegisterException(){
+        super("ERROR => TEACHER NOT REGISTER");
+    }
+
+}
