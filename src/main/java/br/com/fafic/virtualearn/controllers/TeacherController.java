cb557ab54c983b964ae20785c9f38d9669916d86
Kaue Batista <kauecardozo3@gmail.com>
2024-12-03 01:39:06 -0300

    feat: register teacher

---
+++ src/main/java/br/com/fafic/virtualearn/controllers/TeacherController.java
@@ -0,0 +1,64 @@
+package br.com.fafic.virtualearn.controllers;
+
+import br.com.fafic.virtualearn.dao.LoginDAO;
+import br.com.fafic.virtualearn.dao.TeacherDAO;
+import br.com.fafic.virtualearn.exceptions.FieldIsNullException;
+import br.com.fafic.virtualearn.exceptions.InvalidCpfException;
+import br.com.fafic.virtualearn.model.Login;
+import br.com.fafic.virtualearn.model.Student;
+import br.com.fafic.virtualearn.model.Teacher;
+
+import java.time.LocalDate;
+
+public class TeacherController {
+
+    private TeacherDAO teacherDAO = new TeacherDAO();
+
+    private LoginDAO loginDAO = new LoginDAO();
+
+    public boolean createNewTeacher(String name, String email, String phoneNumber, LocalDate date, Login login, String cpf, String matter){
+        try{
+
+            if (fieldValidation(name, email, phoneNumber, date, cpf, matter)){
+                throw new FieldIsNullException();
+            }
+
+            if (cpfValidation(cpf)){
+                throw new InvalidCpfException();
+            }
+
+            Teacher teacher = new Teacher();
+            teacher.setName(name);
+            teacher.setEmail(email);
+            teacher.setDateOfBirth(date);
+            teacher.setPhoneNumber(phoneNumber);
+            teacher.setLogin(login);
+            teacher.setCpf(cpf);
+            teacher.setMatter(matter);
+
+            teacherDAO.registerTeacher(teacher);
+
+            return true;
+
+        }catch (FieldIsNullException e) {
+            System.err.println(e.getMessage());
+            loginDAO.deleteLogin(login);
+            return false;
+        }catch (InvalidCpfException e){
+            System.err.println(e.getMessage());
+            return false;
+        }catch (RuntimeException e){
+            loginDAO.deleteLogin(login);
+            return false;
+        }
+    }
+
+    private boolean fieldValidation(String name, String email, String phoneNumber, LocalDate date, String cpf, String matter){
+        return name.isEmpty() || email.isEmpty() || phoneNumber.isEmpty() || cpf.isEmpty() || date == null || matter.isEmpty();
+    }
+
+    private boolean cpfValidation(String cpf){
+        return cpf.length() < 11;
+    }
+
+}
--- src/main/java/br/com/fafic/virtualearn/view/RegisterTeacherViewController.java
+++ src/main/java/br/com/fafic/virtualearn/view/RegisterTeacherViewController.java
@@ -1,8 +1,19 @@
 package br.com.fafic.virtualearn.view;
 
+import br.com.fafic.virtualearn.controllers.LoginController;
+import br.com.fafic.virtualearn.controllers.TeacherController;
+import br.com.fafic.virtualearn.model.Login;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class RegisterTeacherViewController {
 
@@ -23,5 +34,43 @@ public class RegisterTeacherViewController {
 
     @FXML
     TextField fieldMatter;
+
+    @FXML
+    TextField fieldLogin;
+
+    @FXML
+    TextField fieldPassword;
+
+    @FXML
+    Button registerTeacherButton;
+
+    TeacherController teacherController = new TeacherController();
+
+    LoginController loginController = new LoginController();
+
+    public void onRegisterTeacherButtonClick() throws IOException {
+
+        String login = fieldLogin.getText();
+        String password = fieldPassword.getText();
+
+        Login userLogin = loginController.createLogin(login, password, "student");
+
+        String name = fieldName.getText();
+        String cpf = fieldCpf.getText();
+        String email = fieldEmail.getText();
+        String phoneNumber = fieldPhoneNumber.getText();
+        LocalDate dateOfBirthday = fieldDate.getValue();
+        String matter = fieldMatter.getText();
+
+        if(teacherController.createNewTeacher(name, email,phoneNumber, dateOfBirthday, userLogin, cpf, matter)){
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/pages/dashboard-admin-view.fxml"));
+            Parent loginRoot = fxmlLoader.load();
+            Stage stage = (Stage) registerTeacherButton.getScene().getWindow();
+            Pane mainPane = (Pane) stage.getScene().getRoot();
+            mainPane.getChildren().clear();
+            mainPane.getChildren().add(loginRoot);
+        }
+
+    }
 
 }
