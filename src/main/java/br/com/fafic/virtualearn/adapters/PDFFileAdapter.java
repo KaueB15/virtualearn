016bb8acea6465b41795a54e8e975271341fbc20
evertonc15 <everton.ci@outlook.com>
2024-12-05 08:20:02 -0300

    feat: create generate student pdf

--- src/main/java/br/com/fafic/virtualearn/adapters/PDFFileAdapter.java
+++ src/main/java/br/com/fafic/virtualearn/adapters/PDFFileAdapter.java
@@ -9,44 +9,62 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
+import java.io.File;
 import java.io.IOException;
 
 public class PDFFileAdapter implements FileProcessor {
 
     @Override
     public void generateFile(Rating rating, String filePath, Teacher teacher, Student student) {
-        try (PDDocument document = new PDDocument()) {
-            PDPage page = new PDPage();
-            document.addPage(page);
+        try {
+            String downloadsPath = System.getProperty("user.home") + File.separator + "Downloads";
+            File outputDir = new File(downloadsPath);
 
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
-            contentStream.beginText();
-            contentStream.setFont(PDType1Font.HELVETICA, 12);
-            contentStream.setLeading(14.5f);
-            contentStream.newLineAtOffset(50, 750);
+            if (!outputDir.exists() && !outputDir.mkdirs()) {
+                throw new IOException("Não foi possível criar o diretório de Downloads.");
+            }
 
-            contentStream.showText("Professor: " + teacher.getName());
-            contentStream.newLine();
-            contentStream.showText("Notas do Aluno: " + student.getName());
-            contentStream.newLine();
-            contentStream.showText("Nota 1: " + rating.getR1());
-            contentStream.newLine();
-            contentStream.showText("Nota 2: " + rating.getR2());
-            contentStream.newLine();
-            contentStream.showText("Nota 3: " + rating.getR3());
-            contentStream.newLine();
-            contentStream.showText("Média Final: " + rating.getFinalRating());
-            contentStream.newLine();
+            filePath = downloadsPath + File.separator + "Relatorio-Avaliacao-" + student.getName().replaceAll("\\s+", "_") + ".pdf";
 
-            contentStream.endText();
-            contentStream.close();
+            try (PDDocument document = new PDDocument()) {
+                PDPage page = new PDPage();
+                document.addPage(page);
 
-            document.save(filePath);
-            System.out.println("PDF gerado com sucesso em: " + filePath);
+                PDPageContentStream contentStream = new PDPageContentStream(document, page);
+                contentStream.beginText();
+                contentStream.setFont(PDType1Font.HELVETICA, 32);
+                contentStream.setLeading(14.5f);
+                contentStream.newLineAtOffset(50, 750);
+
+                contentStream.showText("Relatório de Avaliação");
+                contentStream.newLine();
+                contentStream.newLine();
+                contentStream.showText("Aluno: " + student.getName());
+                contentStream.newLine();
+                contentStream.showText("Professor: " + teacher.getName());
+                contentStream.newLine();
+                contentStream.showText("Disciplina: " + teacher.getMatter());
+                contentStream.newLine();
+                contentStream.newLine();
+                contentStream.showText("Notas:");
+                contentStream.newLine();
+                contentStream.showText("Nota 1: " + rating.getR1());
+                contentStream.newLine();
+                contentStream.showText("Nota 2: " + rating.getR2());
+                contentStream.newLine();
+                contentStream.showText("Nota 3: " + rating.getR3());
+                contentStream.newLine();
+                contentStream.showText("Média Final: " + rating.getFinalRating());
+                contentStream.newLine();
+
+                contentStream.endText();
+                contentStream.close();
+
+                document.save(filePath);
+                System.out.println("PDF gerado com sucesso em: " + filePath);
+            }
         } catch (IOException e) {
             throw new RuntimeException("Erro ao gerar o arquivo PDF: " + e.getMessage(), e);
         }
     }
-
-
 }
